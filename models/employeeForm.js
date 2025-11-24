e40f0c4fe91c8ce61bf3ff74e05ee5e650380948
sulemanahmedcs19 <sulemanahmedcs19@gmail.com>
2025-11-25 02:34:24 +0500

    c

--- models/employeeForm.js
+++ models/employeeForm.js
@@ -42,7 +42,6 @@ const Employee = mongoose.model("Employee", EmployeeFormSchema);
 
 // Attendance Schema
 const AttendanceSchema = mongoose.Schema({
-  name: { type: String, ref: "Employee" },
   email: {
     type: String,
     ref: "Employee",
